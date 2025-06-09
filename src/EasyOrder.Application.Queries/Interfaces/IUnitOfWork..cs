ccd3f0bbad83b8a5ccd8f7f99d26604dcb80f6e6
Zeyad Wael <abdallahzeyad337@gmail.com>
2025-06-09 03:04:54 +0300

    Last Change

--- src/EasyOrder.Application.Queries/Interfaces/IUnitOfWork..cs
+++ src/EasyOrder.Application.Queries/Interfaces/IUnitOfWork..cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EasyOrder.Application.Contracts.InterfaceCommon;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace EasyOrderAdmin.Application.Queries.Interfaces
 {
     public interface IUnitOfWork
     {
+
+        IOrderRepository Orders { get; }
+
+        /// <summary>
+        /// Flushes all pending changes to the write database.
+        /// </summary>
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }
